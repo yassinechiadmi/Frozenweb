73a1a89cb490d07a2be43949da2bb567101d1ca7
Rémi HAGE <remi.hage@student.junia.com>
2022-05-17 17:15:29 +0200

    code identation

--- include/nav.php
+++ include/nav.php
@@ -2,29 +2,27 @@
 	<?php session_start();?>
 	<a id="home" href="index.php"><img src="rs/icone.jpg" width=100px height=50px></img></a>
 	<nav> <!--Menu du site-->
-
 		<a href="p3.php">Game - building</a>
 		<a href="Forum.php">Forum</a>
         <a href="FAQ.php">FAQ</a>
 		<a href="p5.php">Ranking - building</a>
 
 		<div id="nav-btn-container">
-		<?php
-		if(isset($_SESSION["username"]) == NULL){
+			<?php
+			if(isset($_SESSION["username"]) == NULL){
+				
+				echo "<a id=\"connexion\" href='login.php'><i class='nav-btn' aria-hidden='true'></i>Login</a>";
+				echo "<a id=\"inscription\" href='register.php'><i class='nav-btn' aria-hidden='true'></i>Register</a>";
 			
-			echo "<a id=\"connexion\" href='login.php'><i class='nav-btn' aria-hidden='true'></i>Login</a>";
-			echo "<a id=\"inscription\" href='register.php'><i class='nav-btn' aria-hidden='true'></i>Register</a>";
-		
-		}
-		else if(isset($_SESSION["username"])){
-			echo "<a id=\"connexion\" href='profile.php'><i class='nav-btn' aria-hidden='true'></i>".$_SESSION['username']."</a>";			
-			echo "<a id=\"deconnexion\" href='logout.php'><i class='nav-btn' aria-hidden='true'></i>Logout</a>";
-		}
-		else{
-			echo "Auth error.";
-		}
-		?>
+			}
+			else if(isset($_SESSION["username"])){
+				echo "<a id=\"connexion\" href='profile.php'><i class='nav-btn' aria-hidden='true'></i>".$_SESSION['username']."</a>";			
+				echo "<a id=\"deconnexion\" href='logout.php'><i class='nav-btn' aria-hidden='true'></i>Logout</a>";
+			}
+			else{
+				echo "Auth error.";
+			}
+			?>
 		</div>
 	</nav>
-
 </header>
