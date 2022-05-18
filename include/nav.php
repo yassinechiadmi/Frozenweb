ebbd4b26e09b2227e7e64da69575d891e3df3154
Rémi HAGE <remi.hage@student.junia.com>
2022-05-18 09:45:12 +0200

    english please

--- include/nav.php
+++ include/nav.php
@@ -11,13 +11,13 @@
 			<?php
 			if(isset($_SESSION["username"]) == NULL){
 				
-				echo "<a id=\"connexion\" href='login.php'><i class='nav-btn' aria-hidden='true'></i>Login</a>";
-				echo "<a id=\"inscription\" href='register.php'><i class='nav-btn' aria-hidden='true'></i>Register</a>";
+				echo "<a id=\"login\" href='login.php'><i class='nav-btn' aria-hidden='true'></i>Login</a>";
+				echo "<a id=\"register\" href='register.php'><i class='nav-btn' aria-hidden='true'></i>Register</a>";
 			
 			}
 			else if(isset($_SESSION["username"])){
-				echo "<a id=\"connexion\" href='profile.php'><i class='nav-btn' aria-hidden='true'></i>".$_SESSION['username']."</a>";			
-				echo "<a id=\"deconnexion\" href='logout.php'><i class='nav-btn' aria-hidden='true'></i>Logout</a>";
+				echo "<a id=\"login\" href='profile.php'><i class='nav-btn' aria-hidden='true'></i>".$_SESSION['username']."</a>";			
+				echo "<a id=\"logout\" href='logout.php'><i class='nav-btn' aria-hidden='true'></i>Logout</a>";
 			}
 			else{
 				echo "Auth error.";
