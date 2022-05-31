23cba38fec3fbfdc061690f9b7329e0f734ef78d
Rémi HAGE <remi.hage@student.junia.com>
2022-05-31 14:15:23 +0200

    upload map et browse maps en construction

--- Forum.php
+++ Forum.php
@@ -1,10 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<?php require("include/head.php");?>
-<body>
-    <?php require("include/nav.php");?>
+<?php require("include/head.php"); ?>
 
-<?php require("include/foot.php")?>
+<body>
+    <?php require("include/nav.php"); ?>
+
+    <div id="case">
+        <form method="post" action="" class="logform">
+
+            <h1> Upload a map </h1>
+            <input type="text" name="map" placeholder="Map data">
+            <input type="text" name="map_name" placeholder="Map name">
+            <input type="submit" name="upload" value="Upload map">
+
+        </form>
+    </div>
+
+    <?php require("include/foot.php") ?>
 </body>
+
 </html>
+
+<?php
+if (isset($_POST["upload"])) {
+    require("backend/interactDB.php");
+    $map = $_POST["map"];
+    $map_name = $_POST["map_name"];
+    $ret = upload_map($map, $map_name);
+    $uid = get_uid();
+    if (!$ret) echo "$ret, $uid";
+}
+?>
--- backend/interactDB.php
+++ backend/interactDB.php
@@ -46,4 +46,29 @@ function edit_password(){
     }
 }
 
+function get_uid() {
+    require('include/connect_db.php');
+    $username = $_SESSION['username'];
+    $res = mysqli_query($connexion, "SELECT `id` FROM `user` WHERE `username` = '$username'");
+    $uid = mysqli_fetch_assoc($res)["id"];
+    return $uid;
+}
+
+function get_pfp() {
+    require('include/connect_db.php');
+    $uid = get_uid();
+    $pfp = mysqli_query($connexion, "SELECT pfp FROM user_pfp WHERE userID = '$uid'");
+    return $pfp;
+}
+
+function upload_map($map, $map_name)
+{
+    require('include/connect_db.php');
+    $uid = get_uid();
+    $_map = mysqli_real_escape_string($connexion, $map);
+    $text = "INSERT INTO `user_map` (`userID`, `map`, `map_name`, `map_id`) VALUES ('$uid', '$_map', '$map_name', NULL)";
+    $req = mysqli_query($connexion, $text);
+    return $req;
+}
+
 ?>
