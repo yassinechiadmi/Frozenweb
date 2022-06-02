8f7123e6f5e230ca82cf760c9384b55ddb9a647d
LimeSku <66lime7@protonmail.com>
2022-06-02 13:55:26 +0200

    nav

--- upload.php
+++ upload.php
@@ -65,6 +65,6 @@ if (isset($_POST["upload"])) {
     $ret = upload_map($map, $map_name);
     $uid = get_uid();
     if (!$ret) echo "$ret, $uid";
-    header("Location:Forum.php");
+    header("Location:upload.php");
 }
 ?>
