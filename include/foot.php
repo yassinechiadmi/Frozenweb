fa8818e0c7f781112f3d484620efd1518f497e2b
Rémi HAGE <remi.hage@student.junia.com>
2022-06-07 10:11:00 +0200

    changement flèche go top

--- include/foot.php
+++ include/foot.php
@@ -1,10 +1,11 @@
+<a href="#top"><img id="go-top" src="rs/arrow-up-solid.svg" style="height: 50px; right: 0;"></a>
+
 <footer>
   <div class="wut">
     <ul>
       <li><a href="https://discord.gg/VN5NPnjkj9" target="_blank"><img id="fb" src="rs/Discord.png" alt="discord" style="height: 50px;"></a></li>
       <li><a href="https://www.instagram.com/froz_glisse/" target="_blank"><img id="insta" src="rs/Instagram_2.png" alt="instagram" style="height: 50px;"></a></li>
       <li><a href="https://twitter.com/Froz_glisse" target="_blank"><img id="tw" src="rs/Twitter.png" alt="tweeter" style="height: 50px;"></a></li>
-      <li><a href="#top"><img id="go-top" src="rs/arrow-up-solid.svg" style="height: 50px; right: 0;"></a></li>
     </ul>
     <!-- <div class="text-center-p-3">
       <a class="text-dark" href="https://www.isen-lille.fr//">© Junia ISEN 2022</a>
